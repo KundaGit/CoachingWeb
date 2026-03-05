324ff1742f8ba45207c26ba1514b98b8188c3b73
Kundan Rajak <137685825+KundaGit@users.noreply.github.com>
2026-03-05 14:43:30 +0530

    Delete unused environment configuration
    Remove commented-out environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: "AIzaSyBgtuWTVQ_GcmEqE0prhzit-JUMP0albGY",
-//     authDomain: "institute-app-9ba44.firebaseapp.com",
-//     projectId: "institute-app-9ba44",
-//     storageBucket: "institute-app-9ba44.firebasestorage.app",
-//     messagingSenderId: "867628725245",
-//     appId: "1:867628725245:web:60821360d6c7a43c6d5f94"
-//   }
-// };
+
